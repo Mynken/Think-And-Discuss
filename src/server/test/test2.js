fd7bbbb1f8ebf78fdf88698543a2f56ab5b07edb
Vladyslav Hrehul <vladyslavherhul@biuro.k2.pl>
2019-12-04 12:09:19 +0100

    test

---
+++ src/server/test/test2.js
@@ -0,0 +1,16 @@
+describe('addTwoNumbers()', function () {
+    it('should add two numbers', function () {
+      
+      // 1. ARRANGE
+      var x = 5;
+      var y = 1;
+      var sum1 = x + y;
+  
+      // 2. ACT
+      var sum2 = 36;
+  
+      // 3. ASSERT
+      expect(sum2).to.be.equal(sum1);
+  
+    });
+  });
